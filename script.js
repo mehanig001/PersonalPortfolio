9d32126ea2545033e42288fada6843c530d35039
mehanig001 <gautammihani258@gmail.com>
2024-07-28 02:52:06 +0530

    changes

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
             $('.navbar').removeClass("sticky");
         }
         
-        // scroll-up button show/hide script
+       
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
         }else{
@@ -15,19 +15,17 @@ $(document).ready(function(){
         }
     });
 
-    // slide-up script
+    
     $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
 
     $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
 
-    // toggle menu/navbar script
+    
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
@@ -50,7 +48,6 @@ $(document).ready(function(){
 
     
 
-    // owl carousel script
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
